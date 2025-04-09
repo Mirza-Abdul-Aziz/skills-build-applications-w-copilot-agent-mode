17b327b3e52c727e8ef61a8d1e0751dadbf63752
Abdul Aziz <mirzaabdulaziz006@gmail.com>
2025-04-09 01:35:40 +0000

    Populated the database...

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,82 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from bson import ObjectId
+from datetime import timedelta
+
+class Command(BaseCommand):
+    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Starting to populate the database...')
+
+        # Clear existing data
+        self.stdout.write('Clearing existing data...')
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create users
+        self.stdout.write('Creating users...')
+        users = [
+            User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='thundergodpassword'),
+            User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='metalgeekpassword'),
+            User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='zerocoolpassword'),
+            User(_id=ObjectId(), username='crashoverride', email='crashoverride@mhigh.edu', password='crashoverridepassword'),
+            User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='sleeptokenpassword'),
+        ]
+        for user in users:
+            user.save()
+        self.stdout.write(f'Created {len(users)} users.')
+
+        # Create teams
+        self.stdout.write('Creating teams...')
+        teams = [
+            Team(_id=ObjectId(), name='Blue Team'),
+            Team(_id=ObjectId(), name='Gold Team'),
+        ]
+        for team in teams:
+            team.save()
+        self.stdout.write(f'Created {len(teams)} teams.')
+
+        # Create activities
+        self.stdout.write('Creating activities...')
+        activities = [
+            Activity(_id=ObjectId(), user=users[0], activity_type='Cycling', duration=timedelta(hours=1)),
+            Activity(_id=ObjectId(), user=users[1], activity_type='Crossfit', duration=timedelta(hours=2)),
+            Activity(_id=ObjectId(), user=users[2], activity_type='Running', duration=timedelta(hours=1, minutes=30)),
+            Activity(_id=ObjectId(), user=users[3], activity_type='Strength', duration=timedelta(minutes=30)),
+            Activity(_id=ObjectId(), user=users[4], activity_type='Swimming', duration=timedelta(hours=1, minutes=15)),
+        ]
+        for activity in activities:
+            activity.save()
+        self.stdout.write(f'Created {len(activities)} activities.')
+
+        # Create leaderboard entries
+        self.stdout.write('Creating leaderboard entries...')
+        leaderboard_entries = [
+            Leaderboard(_id=ObjectId(), user=users[0], score=100),
+            Leaderboard(_id=ObjectId(), user=users[1], score=90),
+            Leaderboard(_id=ObjectId(), user=users[2], score=95),
+            Leaderboard(_id=ObjectId(), user=users[3], score=85),
+            Leaderboard(_id=ObjectId(), user=users[4], score=80),
+        ]
+        for entry in leaderboard_entries:
+            entry.save()
+        self.stdout.write(f'Created {len(leaderboard_entries)} leaderboard entries.')
+
+        # Create workouts
+        self.stdout.write('Creating workouts...')
+        workouts = [
+            Workout(_id=ObjectId(), name='Cycling Training', description='Training for a road cycling event'),
+            Workout(_id=ObjectId(), name='Crossfit', description='Training for a crossfit competition'),
+            Workout(_id=ObjectId(), name='Running Training', description='Training for a marathon'),
+            Workout(_id=ObjectId(), name='Strength Training', description='Training for strength'),
+            Workout(_id=ObjectId(), name='Swimming Training', description='Training for a swimming competition'),
+        ]
+        for workout in workouts:
+            workout.save()
+        self.stdout.write(f'Created {len(workouts)} workouts.')
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
